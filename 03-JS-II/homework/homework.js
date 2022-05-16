ea78d691872bc5fe4c3855560c0d31f63c972c20
ramirorruggeri <ramirorruggeri@gmail.com>
2022-05-16 11:44:40 -0300

    first commit

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -112,7 +112,7 @@ if(numero%3===0){
   return "fizz";
 } else if(numero%5===0){
   return "buzz";
-} else if((numero%3===0)&&(numero%5===0)){
+} else (numero%3===0 && numero%5===0){
   return "fizzbuzz";
 }
   return numero;
