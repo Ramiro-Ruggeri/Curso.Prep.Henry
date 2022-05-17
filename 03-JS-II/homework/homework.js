6b167318bf8601f4a1e52d96da68ee690d8961d1
ramirorruggeri <ramirorruggeri@gmail.com>
2022-05-17 12:50:52 -0300

    first commit

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -147,6 +147,14 @@ function esPrimo(numero) {
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
+  if(numero<2) return false;
+  if(numero===2) return true;
+  for(var i=2; i<numero; i++){
+  if(numero%i===0){
+        return false;
+    }
+  }
+  return true;
 }
 
 function esVerdadero(valor){
@@ -161,7 +169,11 @@ function tablaDelSeis(){
   //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
   //Escribe tu código aquí   
-  
+  var arrayTablaDel6= [];
+    for(var i=o; i<11; i++){
+      arrayTablaDel6.push(6*i)
+    }
+    return arrayTablaDel6;
 }
 
 function tieneTresDigitos(numero){
