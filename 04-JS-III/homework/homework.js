25d6d51833169bcbd3d6fd2c064170b0228e8acf
ramirorruggeri <ramirorruggeri@gmail.com>
2022-05-24 09:51:46 -0300

    first commit

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -156,7 +156,13 @@ function empiezaConNueve(n) {
   //Desarrolle una función que recibe como parámetro un número entero n. Debe retornar true si el entero 
   //inicia con 9 y false en otro caso.
   //Escribe tu código aquí
-  
+  var num = n.toString();
+  for(var i = 0; i<n.toString().length; i++){
+    if(num.charAt(0)==="9"){
+      return true;
+    } 
+}
+  return false;
 }
 
 
@@ -164,7 +170,12 @@ function todosIguales(arreglo) {
   //Escriba la función todosIguales, que indique si todos los elementos de un arreglo son iguales:
   //retornar true, caso contrario retornar false.
   //Escribe tu código aquí  
-  
+  for(var i = 0; i<arreglo.length; i++){
+    if(arreglo[i]!==arreglo[i+1]){
+      return false;
+    }
+    return true;
+  }
 } 
 
 
@@ -173,6 +184,16 @@ function mesesDelAño(array) {
   // "Enero", "Marzo" y "Noviembre", guardarlo en nuevo array y retornarlo.
   //Si alguno de los meses no está, devolver: "No se encontraron los meses pedidos"
   // Tu código:
+  var newArray = [];
+  for ( var i = 0; i < array.length; i++ ) {
+    if( array[i]==="Enero" || array[i]==="Marzo" || array[i]==="Noviembre"){
+    newArray.push(array[i]);
+    }
+  }
+    if(newArray.length < 3){
+    return "No se encontraron los meses pedidos";
+    }
+  return newArray; 
 }
 
 
@@ -180,8 +201,14 @@ function mayorACien(array) {
   //La función recibe un array con enteros entre 0 y 200. Recorrer el array y guardar en un nuevo array sólo los
   //valores mayores a 100 (no incluye el 100). Finalmente devolver el nuevo array.
   // Tu código:
-}
-
+  newArray= [];
+  for ( var i = 0; i<array.length; i++ ) {
+    if(array[i]>100) {
+      newArray.push(array[i]);
+    }
+      return newArray;
+    }
+  }
 
 function breakStatement(numero) {
   //Iterar en un bucle aumentando en 2 el numero recibido hasta un límite de 10 veces.
