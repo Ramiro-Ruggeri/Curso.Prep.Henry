f5d4e3e255c7a36ffc87fdd321cb0d1d18351c75
ramirorruggeri <ramirorruggeri@gmail.com>
2022-05-26 10:32:46 -0300

    first commit

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -137,11 +137,16 @@ function sumarLikesDeUsuario (usuario) {
   // Devuelve la suma
   // Tu código:
   var suma = 0;
-  for ( var i = 0; i < usuario.posts.lenght; i++ ) {
+
+  for(var i = 0; i < usuario.posts.length; i++){
     suma = suma + usuario.posts[i].likes;
   }
+
   return suma;
+
 }
+
+
 
 function agregarMetodoCalculoDescuento (producto) {
   // Agregar un método (función) al objeto "producto" llamado "calcularPrecioDescuento"
@@ -153,7 +158,10 @@ function agregarMetodoCalculoDescuento (producto) {
   // producto.porcentajeDeDescuento -> 0.2 (o simplemente ".2")
   // producto.calcularPrecioDescuento() -> 20 - (20 * 0.2)
   // Tu código:
-
+  producto.calcularPrecioDescuento = function() {
+    return this.precio - (this.precio * porcentajeDeDescuento);
+  }
+  return producto;
 }
 
 // No modificar nada debajo de esta línea
